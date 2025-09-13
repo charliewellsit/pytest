7437e107145bb5610e75c4ed30326ded9fbf24ae
charliewellsit <charliewellsit@gmail.com>
2025-09-13 22:23:38 +1000

    Learnt setup and teardown function and the order of pytest call

---
+++ setUpAndTearDown_test.py
@@ -0,0 +1,22 @@
+def setup_function(function):
+    if function == test1:
+        print('\nsetup function test1')
+    elif function == test2:
+        print('\nsetup function test2')
+    else:
+        print('\nsetup unknown function')
+
+def teardown_function(function):
+    if function == test1:
+        print('\nteardown function test1')
+    elif function == test2:
+        print('\nteardown function test2')
+    else:
+        print('\nteardown unknown function')
+
+def test1():
+    print('executing test1')
+    assert True
+def test2():
+    print('executing test2')
+    assert True
