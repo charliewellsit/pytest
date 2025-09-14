9010e454152b70a29176823571d604253d62b590
charliewellsit <charliewellsit@gmail.com>
2025-09-14 21:20:43 +1000

    Added class level setup/teardown

--- setUpAndTearDown_test.py
+++ setUpAndTearDown_test.py
@@ -1,9 +1,11 @@
+# module-level setup/teardown
 def setup_module(module):
     print('setup module')
 
 def teardown_module(module):
     print('teardown module')
 
+# function-level setup/teardown
 def setup_function(function):
     if function == test1:
         print('\nsetup function test1')
@@ -20,9 +22,36 @@ def teardown_function(function):
     else:
         print('\nteardown unknown function')
 
+# plain test functions
 def test1():
     print('executing test1')
     assert True
+
 def test2():
     print('executing test2')
     assert True
+
+# class-level setup/teardown
+class TestClassExample:
+
+    @classmethod
+    def setup_class(cls):
+        print('\nsetup class TestClassExample')
+
+    @classmethod
+    def teardown_class(cls):
+        print('teardown class TestClassExample')
+
+    def setup_method(self, method):
+        print(f'\nsetup method {method.__name__}')
+
+    def teardown_method(self, method):
+        print(f'teardown method {method.__name__}')
+
+    def test3(self):
+        print('executing test3')
+        assert True
+
+    def test4(self):
+        print('executing test4')
+        assert True
