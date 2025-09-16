7132eecd42358bf8e55f30a7952a4bdcbc306ebd
charliewellsit <charliewellsit@gmail.com>
2025-09-16 11:26:34 +1000

    Added comments for better understanding

--- pytestFixturesSetup_test.py
+++ pytestFixturesSetup_test.py
@@ -13,4 +13,19 @@ def test2():
     print('test2')
     assert True
 
+"""
+output:
+============================= test session starts =============================
+collected 2 items
+
+setup
+test1
+.
+setup
+test2
+.
+
+============================== 2 passed in 0.01s ==============================
+"""
+
 # You can do '@pytest.fixture(autouse=True)' to have setup run at all tests.
